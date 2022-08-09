355d8c15e162969ca74c6fac1a792bf8ba38da4c
Hesam Bayat <hesam.bayat@gmail.com>
2022-08-09 11:27:47 +0400

    :white_check_mark: Add Keypad tests

---
+++ src/componentes/keypad/index.test.tsx
@@ -0,0 +1,15 @@
+import { render, screen } from '@testing-library/react'
+import { parse } from '../../hooks/useTranslationSource'
+import * as mock from '../../mock'
+import Keypad from './index'
+
+describe('component keypad', () => {
+  const [source] = mock.data.map(parse)
+  render(<Keypad {...source} />)
+  const keys = screen.getAllByTestId('key')
+  const totalKeys = source.grid.reduce((total, row) => total + row.length, 0)
+
+  test('should keys be rendered', () => {
+    expect(keys.length).toEqual(totalKeys)
+  })
+})
