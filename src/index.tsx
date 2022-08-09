818ab303307d29209be6936afd32c5d865c589d8
Hesam Bayat <hesam.bayat@gmail.com>
2022-08-09 11:06:19 +0400

    :lipstick: Add style files

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import reportWebVitals from './reportWebVitals'
-import './style'
+import './styles'
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
---
+++ src/styles/index.js
@@ -0,0 +1,6 @@
+import './variables.css'
+import './reset.css'
+
+import './typography.css'
+import './components.css'
+import './layouts.css'
