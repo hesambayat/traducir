084d391e1332c8c2fd14041fd56872675c8c5ad3
Hesam Bayat <hesam.bayat@gmail.com>
2022-08-09 11:23:05 +0400

    :sparkles: Add `Keypad` component

---
+++ src/componentes/keypad/index.tsx
@@ -0,0 +1,95 @@
+import { useCallback, useReducer } from 'react'
+import { Move, SourceParsed } from '../../types'
+import Key from '../key'
+
+interface KeypadProps extends SourceParsed {
+  onDone?: () => void
+}
+
+interface KeypadReducerAction {
+  type: string
+  target: number[]
+}
+
+const reducer = (state: number[][], action: KeypadReducerAction) => {
+  switch (action.type) {
+    case Move.DOWN:
+      return [action.target]
+    case Move.MOVE:
+      const [first] = state
+      const [ax, ay] = first
+      const [bx, by] = action.target
+      const hor = Math.abs(ax - bx)
+      const ver = Math.abs(ay - by)
+      const length = hor && ver ? Math.min(hor, ver) : hor || ver
+      if (!length) {
+        return [action.target]
+      }
+
+      return [
+        first,
+        ...Array.from({ length }, (_, i) => [((i + 1) * Math.sign(ax - bx) * -1) + ax, ((i + 1) * Math.sign(ay - by) * -1) + ay])
+      ]
+    case Move.UP:
+      return []
+    default:
+      throw new Error('Action not supported')
+  }
+}
+
+const Keypad = ({ grid, map, onDone }: KeypadProps) => {
+  const [trace, dispatch] = useReducer(reducer, [])
+  const onStart = useCallback((target: KeypadReducerAction['target']) => {
+    return () => dispatch({ type: Move.DOWN, target })
+  }, [dispatch])
+  const onEnd = useCallback((target: KeypadReducerAction['target']) => {
+    return () => {
+      const traceString = trace.slice().map(n => n.toString()).sort().toString()
+      map.find((check) => {
+        const checkString = check.map(n => n.toString()).sort().toString()
+        if (checkString === traceString) {
+          onDone && onDone()
+          return true
+        }
+        
+        return false
+      })
+
+      dispatch({ type: Move.UP, target })
+    }
+  }, [dispatch, trace, map, onDone])
+  const onMove = useCallback((target: KeypadReducerAction['target']) => {
+    return () => {
+      if (trace.length) {
+        dispatch({ type: Move.MOVE, target })
+      }
+    }
+  }, [dispatch, trace])
+  const isSelected = useCallback((target: KeypadReducerAction['target']) => {
+    return trace.map(n => n.toString()).includes(target.toString())
+  }, [trace])
+
+  return (
+    <div className="keypad">
+      {grid.map((row, rowIndex) => (
+        <div key={`keyrow-${rowIndex}`} className="keypad-row">
+          {row.map((letter, columnIndex) => {
+            const target = [columnIndex, rowIndex]
+            return (
+              <Key 
+                key={`${letter}-${columnIndex}`} 
+                label={letter}
+                selected={isSelected(target)}
+                onStart={onStart(target)}
+                onEnd={onEnd(target)}
+                onMove={onMove(target)}
+              />
+            )
+          })}
+        </div>
+      ))}
+    </div>
+  )
+}
+
+export default Keypad
